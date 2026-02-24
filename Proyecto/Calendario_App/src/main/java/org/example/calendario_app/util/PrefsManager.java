6484c0d6bfb6fc684bcf16e429d1963bae9525c9
Terrorismo-en-Paraguay <5519473@alu.murciaeduca.es>
2026-02-24 12:06:56 +0100

    si

--- Proyecto/Calendario_App/src/main/java/org/example/calendario_app/util/PrefsManager.java
+++ Proyecto/Calendario_App/src/main/java/org/example/calendario_app/util/PrefsManager.java
@@ -4,8 +4,8 @@ import java.util.prefs.Preferences;
 
 public class PrefsManager {
     private static final String KEY_EMAIL = "user_email";
-    private static final String KEY_PASSWORD = "user_password"; // Note: Storing plain text password is not secure; for
-                                                                // demo only.
+    private static final String KEY_PASSWORD = "user_password";
+
 
     private static final Preferences prefs = Preferences.userNodeForPackage(PrefsManager.class);
 
